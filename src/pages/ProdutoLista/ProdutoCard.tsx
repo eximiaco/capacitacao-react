325a3cbc9279497324189f3619d46e8ff2093c2c
Evandro Oliveira <Evandro.oliveira@eximia.co>
2025-02-11 16:45:45 -0300

    Adiciona classes css de apoio

--- src/pages/ProdutoLista/ProdutoCard.tsx
+++ src/pages/ProdutoLista/ProdutoCard.tsx
@@ -3,13 +3,18 @@ import { Produto } from "../../models/Produto"
 type Props = {
   produto: Produto
 }
-export const ProdutoCard = ({produto}: Props) => {
+export const ProdutoCard = ({ produto }: Props) => {
   return (
     <div className="card card-produto">
       <h4>{produto.title}</h4>
       <img src={produto.image} />
       <p>{produto.description.substring(0, 90)}...</p>
-      <button>Favoritar</button>
+
+      <div className="actions">
+        <button>Favoritar</button>
+        <button className="danger">Favoritar</button>
+
+      </div>
     </div>
   )
 }
