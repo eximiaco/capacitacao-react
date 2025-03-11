d066246207e3115c78a52152e7bde2613928fa51
Evandro Oliveira <Evandro.oliveira@eximia.co>
2025-03-11 01:35:49 -0300

    Organiza formatação da página

--- src/features/ecommerce/pages/ProdutoCriar/index.tsx
+++ src/features/ecommerce/pages/ProdutoCriar/index.tsx
@@ -2,7 +2,7 @@ import { Controller } from "react-hook-form";
 import { TotalFavoritos } from "@/features/ecommerce/components/TotalFavoritos";
 import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
-import { Stack, TextField } from "@mui/material";
+import { Button, Card, Stack, TextField } from "@mui/material";
 import { useProdutoCriar } from "./useProdutCriar";
 import { useLoadingState } from "@/core/providers/LoadingContext/useLoadingState";
 import { FormProduto } from "./form.schema";
@@ -34,92 +34,93 @@ export const ProdutoCriarPage = () => {
         <TotalFavoritos />
       </Stack>
 
-      <div className="content" style={{ marginTop: 28 }}>
+      <Card sx={{padding: 4, marginTop: 2}}>
+        <Stack>
+          <form onSubmit={handleSubmit(onSubmitHandler)}>
+            <div className="field">
+              <label>Título</label>
+              <Controller
+                name="title"
+                defaultValue=""
+                control={control}
+                render={({ field }) =>
+                  <TextField
+                    error={!!formState.errors.title}
+                    helperText={formState.errors.title?.message}
+                    {...field}
+                  />}
+              />
+            </div>
 
-        <form onSubmit={handleSubmit(onSubmitHandler)}>
-          <div className="field">
-            <label>Título</label>
-            <Controller
-              name="title"
-              defaultValue=""
-              control={control}
-              render={({ field }) =>
-                <TextField
-                  error={!!formState.errors.title}
-                  helperText={formState.errors.title?.message}
-                  {...field}
-                />}
-            />
-          </div>
+            <div className="field">
+              <label>URL imagem</label>
+              <Controller
+                name="thumbnail"
+                defaultValue=""
+                control={control}
+                render={({ field }) =>
+                  <TextField
+                    error={!!formState.errors.thumbnail}
+                    helperText={formState.errors.thumbnail?.message}
+                    {...field}
+                  />}
+              />
+            </div>
 
-          <div className="field">
-            <label>URL imagem</label>
-            <Controller
-              name="thumbnail"
-              defaultValue=""
-              control={control}
-              render={({ field }) =>
-                <TextField
-                  error={!!formState.errors.thumbnail}
-                  helperText={formState.errors.thumbnail?.message}
-                  {...field}
-                />}
-            />
-          </div>
+            <div className="field">
+              <label>Preço</label>
+              <Controller
+                name="price"
+                defaultValue=""
+                control={control}
+                render={({ field }) =>
+                  <TextField
+                    error={!!formState.errors.price}
+                    helperText={formState.errors.price?.message}
+                    {...field}
+                  />}
+              />
+            </div>
 
-          <div className="field">
-            <label>Preço</label>
-            <Controller
-              name="price"
-              defaultValue=""
-              control={control}
-              render={({ field }) =>
-                <TextField
-                  error={!!formState.errors.price}
-                  helperText={formState.errors.price?.message}
-                  {...field}
-                />}
-            />
-          </div>
+            <div className="field">
+              <label>Categoria</label>
+              <Controller
+                name="category"
+                defaultValue=""
+                control={control}
+                render={({ field }) =>
+                  <TextField
+                    error={!!formState.errors.category}
+                    helperText={formState.errors.category?.message}
+                    {...field}
+                  />}
+              />
+            </div>
 
-          <div className="field">
-            <label>Categoria</label>
-            <Controller
-              name="category"
-              defaultValue=""
-              control={control}
-              render={({ field }) =>
-                <TextField
-                  error={!!formState.errors.category}
-                  helperText={formState.errors.category?.message}
-                  {...field}
-                />}
-            />
-          </div>
+            <div className="field">
+              <label>Descrição</label>
+              <Controller
+                name="description"
+                defaultValue=""
+                control={control}
+                render={({ field }) =>
+                  <TextField
+                    multiline
+                    rows={5}
+                    error={!!formState.errors.description}
+                    helperText={formState.errors.description?.message}
+                    {...field}
+                  />}
+              />
+            </div>
 
-          <div className="field">
-            <label>Descrição</label>
-            <Controller
-              name="description"
-              defaultValue=""
-              control={control}
-              render={({ field }) =>
-                <TextField
-                  multiline
-                  rows={5}
-                  error={!!formState.errors.description}
-                  helperText={formState.errors.description?.message}
-                  {...field}
-                />}
-            />
-          </div>
-
-          <div className="form-actions">
-            <button type="submit">Criar produto</button>
-            <button onClick={onCancel} className="secondary">Cancelar</button>
-          </div>
-        </form>
-      </div>
+            <Stack direction="row" justifyContent="space-between">
+              <Button onClick={onCancel} variant="outlined">Cancelar</Button>
+              <Button type="submit">Criar produto</Button>
+            </Stack>
+          </form>
+        </Stack>
+      </Card>
     </>
   )
 }
